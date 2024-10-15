96b5fa76aebc620fe392d147a2213166713c91f5
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2024-10-15 20:46:19 -0300

    get user only when is not the logged one

--- client/src/hooks/api/useQueryUserById.tsx
+++ client/src/hooks/api/useQueryUserById.tsx
@@ -1,17 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
 import { getUserById } from '../../services'
 import { User } from '../../models'
+import { useUserStore } from '../stores'
 
 const useQueryUserById = (id: User['id']) => {
+   const { currentUser } = useUserStore()
+
    const { data, isError, isLoading } = useQuery({
-      queryKey: ['getUserById', id],
+      queryKey: ['get-user', id],
       queryFn: () => getUserById(id),
       staleTime: 20 * 60 * 1000, //20min
+      enabled: id !== currentUser?.id,
       retry: 1,
    })
 
    return {
-      user: data?.user,
+      user: id !== currentUser?.id ? data?.user : currentUser,
       isError,
       isLoading,
    }
